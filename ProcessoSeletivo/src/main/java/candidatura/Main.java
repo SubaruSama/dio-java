054e5ee9e9c408540edc8c25862abf3dbdb36507
SubaruSama <andresfauque@gmail.com>
2025-01-28 18:37:01 -0300

    Adicionando algumas funcionalidades. Todos os métodos estão static, melhorar mais pra frente.

--- ProcessoSeletivo/src/main/java/candidatura/Main.java
+++ ProcessoSeletivo/src/main/java/candidatura/Main.java
@@ -1,7 +1,44 @@
 package candidatura;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> candidatos = selecaoCandidatos();
+
+        for (String candidato : candidatos)
+            System.out.println(candidato);
+    }
+
+    public static List<String> selecaoCandidatos() {
+        String[] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Paula", "Marcos", "Daniel", "Daniela"};
+        int candidatosSelecionados = 0;
+        List<String> candidatosSelecionadosLista = new ArrayList<>();
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " propos o valor de " + salarioPretendido);
+
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionadosLista.add(candidato);
+                candidatosSelecionados++;
+            }
+
+            candidatosAtual++;
+        }
+
+        return candidatosSelecionadosLista;
+
+    }
+
+    public static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 }
