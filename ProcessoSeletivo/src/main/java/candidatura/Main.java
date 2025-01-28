a43e39c93c80a1423fd910a6e17fd63d9d231679
SubaruSama <andresfauque@gmail.com>
2025-01-28 20:11:04 -0300

    Finalizando o projeto.

--- ProcessoSeletivo/src/main/java/candidatura/Main.java
+++ ProcessoSeletivo/src/main/java/candidatura/Main.java
@@ -2,17 +2,51 @@ package candidatura;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args) {
         List<String> candidatos = selecaoCandidatos();
 
+        System.out.println("  -- Candidatos selecionados:");
         for (String candidato : candidatos)
-            System.out.println(candidato);
+            System.out.println("  -- " + candidato);
+
+        for (String candidato : candidatos)
+            entrarEmContatoComCandidatos(candidato);
+    }
+
+    // Método para simular o RH entrando em contato com os candidatos
+    static void entrarEmContatoComCandidatos(String candidato) {
+
+        int tentativasRealizdas = 3;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+
+            if (continuarTentando)
+                tentativasRealizdas++;
+            else
+                System.out.println("Contato realizado com o candidato " + candidato);
+        } while (continuarTentando && tentativasRealizdas < 3);
+
+        if (atendeu)
+            System.out.println("Contato realizado com o candidato " + candidato + " na tentativa de numero " + tentativasRealizdas);
+        else
+            System.out.println("Não foi possível contato com o candidato " + candidato);
+    }
+
+    // Método para simular o candidato atendendo o telefonema do RH
+    static boolean atender() {
+        return new Random().nextInt(3) == 1;
     }
 
     public static List<String> selecaoCandidatos() {
+        System.out.println("Fase de seleção de candidatos:");
         String[] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Paula", "Marcos", "Daniel", "Daniela"};
         int candidatosSelecionados = 0;
         List<String> candidatosSelecionadosLista = new ArrayList<>();
